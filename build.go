d85232f4e00ceb2f29444da8c6441b45b6573226
zxz <zxz@likeit.cn>
2019-04-01 15:07:29 +0800

    更新build文件

--- build.go
+++ build.go
@@ -30,13 +30,20 @@ type depSpec struct {
 }
 
 type buildConf struct {
-	PRE_COMPILE_EXEC  []string
-	POST_COMPILE_EXEC []string
-	EXTRA_LDFLAGS     string
-	BUILD_TAGS        string
+	PRE_BUILD_EXEC []string
+	PRE_DEBUG_EXEC []string
+	POST_COMP_EXEC []string
+	EXTRA_LDFLAGS  string
+	EXTRA_ENVIRON  []string
+	BUILD_TAGS     string
 }
 
-var PROJ_ROOT, CMD string
+var bc buildConf
+
+var (
+	PROJ_ROOT, PROJ_NAME, CMD string
+	PROJ_ARGS                 []string
+)
 
 func getDepends() (deps []depSpec) {
 	f, err := os.Open("depends")
@@ -70,6 +77,9 @@ func getDepends() (deps []depSpec) {
 				gs.isGit = false
 			}
 		}
+		if strings.HasSuffix(gs.path, ".git") {
+			gs.path = gs.path[:len(gs.path)-4]
+		}
 		gs.commit = "HEAD"
 		gs.branch = "master"
 		if len(spec) == 2 {
@@ -89,7 +99,7 @@ func getDepends() (deps []depSpec) {
 	return
 }
 
-func updDepends(deps []depSpec, full bool) (depRoots []string) {
+func updDepends(deps []depSpec, mode int) (depRoots []string) {
 	PROJ_SRC := path.Join(PROJ_ROOT, "src")
 	rs := make(map[string]int)
 	for _, repo := range deps {
@@ -99,14 +109,25 @@ func updDepends(deps []depSpec, full bool) (depRoots []string) {
 		cd := path.Join(PROJ_SRC, repo.path)
 		fi, err := os.Stat(path.Join(cd, ".git"))
 		if err == nil && fi.IsDir() {
-			if !full {
+			switch mode {
+			case 1: //force re-sync
+				err := exec.Command("rm", "-fr", cd).Run()
+				if err != nil {
+					fmt.Printf("\nFAILED: rm -fr %s (%v)\n", cd, err)
+					os.Exit(1)
+				}
+			case 2: //update repo
+				if repo.isGit {
+					_, err = exec.Command("git", "-C", cd, "pull").Output()
+					assert(err)
+				} else {
+					assert(run("go", "get", "-u", repo.url))
+				}
+				fmt.Println(" ...updated")
+				continue
+			default: //skip if exists
 				fmt.Println(" ...skipped")
 				continue
-			}
-			err := exec.Command("rm", "-fr", cd).Run()
-			if err != nil {
-				fmt.Printf("\nFAILED: rm -fr %s (%v)\n", cd, err)
-				os.Exit(1)
 			}
 		}
 		fmt.Printf("\n")
@@ -119,7 +140,16 @@ func updDepends(deps []depSpec, full bool) (depRoots []string) {
 			args = append(args, "--depth", "1")
 		}
 		args = append(args, "--branch", repo.branch, "--single-branch", cd)
-		assert(exec.Command("git", args...).Run())
+		_, err = exec.Command("git", args...).Output()
+		if err != nil {
+			switch err.(type) {
+			case *exec.ExitError:
+				fmt.Println(string(err.(*exec.ExitError).Stderr))
+			default:
+				fmt.Println(err)
+			}
+			os.Exit(1)
+		}
 		if repo.commit != "HEAD" {
 			os.Chdir(cd)
 			assert(exec.Command("git", "checkout", repo.commit).Run())
@@ -187,8 +217,9 @@ func getGitInfo() (branch, hash string, revisions int) {
 	return
 }
 
-func parseConf() (bc buildConf, err error) {
-	f, err := os.Open(path.Join(PROJ_ROOT, "build.conf"))
+func parseConf() (err error) {
+	conf := path.Join(PROJ_ROOT, "src", PROJ_NAME, "build.conf")
+	f, err := os.Open(conf)
 	if err != nil {
 		if os.IsNotExist(err) {
 			fmt.Println("[build.conf] not found, continue with defaults...")
@@ -199,7 +230,7 @@ func parseConf() (bc buildConf, err error) {
 	defer f.Close()
 	getCmd := func(cmdline string) []string {
 		cmdline = strings.TrimSpace(cmdline)
-		w := "/ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0192456789."
+		w := "/ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789."
 		if strings.ContainsAny(cmdline[0:1], w) {
 			return strings.Split(cmdline, " ")
 		}
@@ -212,17 +243,21 @@ func parseConf() (bc buildConf, err error) {
 			continue
 		}
 		kv := strings.SplitN(line, "=", 2)
-		if len(kv) != 2 {
+		if len(kv) != 2 || kv[0] == "" || kv[1] == "" {
 			continue
 		}
 		key := strings.TrimSpace(kv[0])
 		switch strings.ToUpper(key) {
-		case "PRE_COMPILE_EXEC":
-			bc.PRE_COMPILE_EXEC = getCmd(kv[1])
-		case "POST_COMPILE_EXEC":
-			bc.POST_COMPILE_EXEC = getCmd(kv[1])
+		case "PRE_BUILD_EXEC":
+			bc.PRE_BUILD_EXEC = getCmd(kv[1])
+		case "PRE_DEBUG_EXEC":
+			bc.PRE_DEBUG_EXEC = getCmd(kv[1])
+		case "POST_COMP_EXEC":
+			bc.POST_COMP_EXEC = getCmd(kv[1])
 		case "EXTRA_LDFLAGS":
 			bc.EXTRA_LDFLAGS = strings.TrimSpace(kv[1])
+		case "EXTRA_ENVIRON":
+			bc.EXTRA_ENVIRON = getCmd(kv[1])
 		case "BUILD_TAGS":
 			bc.BUILD_TAGS = strings.TrimSpace(kv[1])
 		default:
@@ -235,116 +270,183 @@ func parseConf() (bc buildConf, err error) {
 }
 
 func run(args ...string) (err error) {
-	exe := args[0]
-	if exe == "go" {
-		exe = "/usr/local/go/bin/go"
-	}
-	cmd := exec.Command(exe, args[1:]...)
+	cmd := exec.Command(args[0], args[1:]...)
 	for _, e := range os.Environ() {
-		if strings.HasPrefix(e, "PATH=") {
-			cmd.Env = append(cmd.Env, e)
-			break
-		}
+		cmd.Env = append(cmd.Env, e)
 	}
-	cmd.Env = append(cmd.Env, "GOPATH="+PROJ_ROOT, "GOBUILD_MODE="+CMD)
+	cmd.Env = append(cmd.Env, "GOPATH="+PROJ_ROOT)
+	if args[0] == "go" && args[1] == "build" {
+		if CMD == "win" {
+			cmd.Env = append(cmd.Env, "GOOS=windows")
+		} else if CMD == "arm" {
+			cmd.Env = append(cmd.Env, "GOARCH=arm", "GOARM=7")
+		}
+		cmd.Env = append(cmd.Env, bc.EXTRA_ENVIRON...)
+	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
 }
 
-func parseCmdline() (string, string, []string) {
+func parseCmdline() {
 	PROJ_ROOT = os.Args[len(os.Args)-1]
-	fs, err := filepath.Glob(PROJ_ROOT + "/src/*/*.go")
-	assert(err)
-	mains := make(map[string]bool)
-	for _, fn := range fs {
-		func() {
-			f, err := os.Open(fn)
-			assert(err)
-			defer f.Close()
-			buf := make([]byte, 64)
-			n, err := f.Read(buf)
-			assert(err)
-			pkg := string(buf[:n])
-			if strings.HasPrefix(pkg, "package main") {
-				mains[path.Base(path.Dir(fn))] = true
-			}
-		}()
+	if CMD == "sync" {
+		return
 	}
-	PROJ_NAME := path.Base(PROJ_ROOT)
-	PROJ_ARGS := os.Args[1 : len(os.Args)-1]
-	if len(PROJ_ARGS) > 0 && mains[PROJ_ARGS[0]] {
+	var mains, main []string
+	filepath.Walk(PROJ_ROOT, func(path string, info os.FileInfo,
+		err error) error {
+		if err == nil && !info.IsDir() && strings.HasSuffix(path, ".go") {
+			func() {
+				dir := filepath.Dir(path[len(PROJ_ROOT)+5:])
+				if dir == "." {
+					return
+				}
+				f, err := os.Open(path)
+				if err != nil {
+					return
+				}
+				defer f.Close()
+				score := 0
+				lines := bufio.NewScanner(f)
+				for lines.Scan() {
+					line := lines.Text()
+					if strings.HasPrefix(line, "package main") {
+						score++
+					}
+					if strings.HasPrefix(line, "func main()") {
+						score++
+					}
+					if score > 1 {
+						break
+					}
+				}
+				if score > 1 {
+					mains = append(mains, dir)
+				}
+			}()
+		}
+		return nil
+	})
+	if len(mains) == 0 {
+		fmt.Println("No target found (require a func main() in package main)")
+		os.Exit(1)
+	}
+	PROJ_NAME = path.Base(PROJ_ROOT)
+	PROJ_ARGS = os.Args[1 : len(os.Args)-1]
+	if len(PROJ_ARGS) > 0 && !strings.HasPrefix(PROJ_ARGS[0], "-") {
 		PROJ_NAME = PROJ_ARGS[0]
 		PROJ_ARGS = PROJ_ARGS[1:]
 	}
-	if !mains[PROJ_NAME] {
-		if len(mains) == 1 {
-			for m := range mains {
-				PROJ_NAME = m
-			}
-		} else {
-			fmt.Println("Program entry (package main) not match.")
-			if len(mains) > 0 {
-				fmt.Print("Available entries:")
-				for m := range mains {
-					fmt.Printf(" [%s]", m)
-				}
-				fmt.Println()
-				os.Exit(0)
-			}
+	for _, m := range mains {
+		if m == PROJ_NAME {
+			main = []string{m}
+			break
+		}
+		if strings.Contains(m, PROJ_NAME) {
+			main = append(main, m)
 		}
 	}
-	return PROJ_ROOT, PROJ_NAME, PROJ_ARGS
+	switch len(main) {
+	case 0:
+		fmt.Printf("Invalid sub-project [%s], valid names:", PROJ_NAME)
+		for _, m := range mains {
+			fmt.Printf(" [%s]", m)
+		}
+		fmt.Println()
+		os.Exit(1)
+	case 1:
+		PROJ_NAME = main[0]
+	default:
+		fmt.Print("Ambiguous sub-project name, matched:")
+		for _, m := range main {
+			fmt.Printf(" [%s]", m)
+		}
+		fmt.Println()
+		os.Exit(1)
+	}
 }
+
 func main() {
+	defer func() {
+		if e := recover(); e != nil {
+			fmt.Println("PANIC:", e.(error).Error())
+			os.Exit(1)
+		}
+	}()
 	CMD = path.Base(os.Args[0])
-	PROJ_ROOT, PROJ_NAME, PROJ_ARGS := parseCmdline()
+	parseCmdline()
 	assert(os.Chdir(PROJ_ROOT))
-	depRoots := updDepends(getDepends(), CMD == "sync")
+	var syncMode int
+	if len(os.Args) > 1 {
+		switch os.Args[1] {
+		case "-force":
+			syncMode = 1
+		case "-update":
+			syncMode = 2
+		}
+	}
+	depRoots := updDepends(getDepends(), syncMode)
 	updGitIgnore(depRoots)
 	if CMD == "sync" {
 		return
 	}
 	fmt.Println()
-	bc, err := parseConf()
+	err := parseConf()
 	if err != nil {
 		fmt.Printf("parseConf: %s\n", err)
-		return
+		os.Exit(1)
 	}
-	if len(bc.PRE_COMPILE_EXEC) > 0 {
-		fmt.Printf("PRE_COMPILE_EXEC: %s\n", strings.Join(bc.PRE_COMPILE_EXEC,
-			" "))
-		err = run(bc.PRE_COMPILE_EXEC...)
+	scripts := bc.PRE_BUILD_EXEC
+	if CMD == "run" && len(bc.PRE_DEBUG_EXEC) > 0 {
+		scripts = bc.PRE_DEBUG_EXEC
+	}
+	if len(scripts) > 0 {
+		fmt.Printf("PRE_COMP_EXEC: %s\n", strings.Join(scripts, " "))
+		err = run(scripts...)
 		if err != nil {
-			fmt.Printf("PRE_COMPILE_EXEC: %s\n", err)
-			return
+			fmt.Printf("PRE_COMP_EXEC: %s\n", err)
+			os.Exit(1)
 		}
 	}
 	branch, hash, revs := getGitInfo()
-	args := []string{"go", "install"}
+	args := []string{"go"}
+	if CMD != "build" && CMD != "run" {
+		exe := PROJ_NAME
+		if CMD == "win" {
+			exe += ".exe"
+		}
+		args = append(args, "build", "-o", "bin/"+exe)
+	} else {
+		args = append(args, "install")
+	}
+	//add race condiction detection for debugging purpose
+	if CMD == "run" {
+		args = append(args, "-race")
+	}
 	if len(bc.BUILD_TAGS) > 0 {
 		args = append(args, "-tags", bc.BUILD_TAGS)
 	}
-	ldflags := fmt.Sprintf(`%s -X main._G_BRANCH=%s -X main._G_HASH=%s
+	ldflags := fmt.Sprintf(`%s -s -w -X main._G_BRANCH=%s -X main._G_HASH=%s
 		-X main._G_REVS=%d -X main._BUILT_=%d`, bc.EXTRA_LDFLAGS, branch,
 		hash, revs, time.Now().Unix())
 	args = append(args, "-ldflags", ldflags, PROJ_NAME)
 	err = run(args...)
 	if err != nil {
 		fmt.Printf("COMPILE: %s\n", err)
-		return
+		os.Exit(1)
 	}
-	if len(bc.POST_COMPILE_EXEC) > 0 {
-		fmt.Printf("POST_COMPILE_EXEC: %s\n", strings.Join(
-			bc.POST_COMPILE_EXEC, " "))
-		err = run(bc.POST_COMPILE_EXEC...)
+	if len(bc.POST_COMP_EXEC) > 0 {
+		fmt.Printf("POST_COMP_EXEC: %s\n", strings.Join(bc.POST_COMP_EXEC, " "))
+		err = run(bc.POST_COMP_EXEC...)
 		if err != nil {
-			fmt.Printf("POST_COMPILE_EXEC: %s", err)
-			return
+			fmt.Printf("POST_COMP_EXEC: %s", err)
+			os.Exit(1)
 		}
 	}
 	if CMD == "run" {
+		fmt.Println("\nRUNNING:")
 		args := []string{path.Join(PROJ_ROOT, "bin", PROJ_NAME)}
 		args = append(args, PROJ_ARGS...)
 		assert(run(args...))
