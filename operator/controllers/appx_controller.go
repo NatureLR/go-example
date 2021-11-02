cb7c56a98340d511aa44a564c71c38ab8c2224a6
NatureLR <1127711564@qq.com>
2021-11-02 18:51:41 +0800

    oper:删除无用代码

--- operator/controllers/appx_controller.go
+++ operator/controllers/appx_controller.go
@@ -198,22 +198,3 @@ func (r *AppxReconciler) SetupWithManager(mgr ctrl.Manager) error {
 }
 
 func int32Ptr(i int32) *int32 { return &i }
-
-func containsString(slice []string, s string) bool {
-	for _, item := range slice {
-		if item == s {
-			return true
-		}
-	}
-	return false
-}
-
-func removeString(slice []string, s string) (result []string) {
-	for _, item := range slice {
-		if item == s {
-			continue
-		}
-		result = append(result, item)
-	}
-	return
-}
